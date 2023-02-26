f7528b952c8ae6acef26a4c945188b382d73837b
ValeriaKul <lera.dribnyuk@mail.ru>
2023-02-26 19:43:10 +0200

    changed link to GH

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="header">
       <h2>
         Link to the code:{" "}
-        <a href="https://github.com/ValeriaKul/Algoritms-JS">LINK</a>
+        <a href="https://github.com/ValeriaKul/HW-15-Feb-2023-People">LINK</a>
       </h2>
     </div>
   );
