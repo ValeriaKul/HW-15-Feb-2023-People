e9aa2a68fc5c50d1e3b5be127fc96b30949fa2dd
ValeriaKul <lera.dribnyuk@mail.ru>
2023-02-26 19:41:48 +0200

    HW 15 Feb 2023

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h2>
+        Link to the code:{" "}
+        <a href="https://github.com/ValeriaKul/Algoritms-JS">LINK</a>
+      </h2>
+    </div>
+  );
+}
---
+++ src/components/Male/Male.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import People from "../People/People";
+
+export default function Male({ newPeople }) {
+  const man = newPeople.filter((men) => men.sex === "male");
+  console.log(man);
+  return (
+    <div className="cont_male">
+      <p className="p_man">Man</p>
+      <div className="div_male">
+        {man.map((men) => (
+          <People key={men.id} {...men} />
+        ))}
+      </div>
+    </div>
+  );
+}
